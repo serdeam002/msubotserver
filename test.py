5bbe9b6fa705cb8b248e926bedf56c1c4f2b3d3c
serdeam002 <thanawanphansa2020@gmail.com>
2023-11-26 21:26:52 +0700

    fixurl

--- test.py
+++ test.py
@@ -1,46 +1,39 @@
-from flask import Flask, jsonify, request
-from sqlalchemy import create_engine, text
 import requests
-import os  # Import the 'os' module
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Replace 'mysql://username:password@hostname:port/database' with your actual connection string
-connection_string = 'mysql://w1l23s172nhtc09c7ora:pscale_pw_6LbblmUuoX9e9EwH2IuMHnNVtkZqczGpzyNFTBlB0H3@aws.connect.psdb.cloud:3306/msubotdb'
-engine = create_engine(connection_string)
+# Replace 'YOUR_PLANETSCALE_API_KEY', 'YOUR_PLANETSCALE_ORG', and 'YOUR_PLANETSCALE_DB' with your actual values
+planet_scale_api_key = 'pscale_tkn_AUfnc8OpiwAZx7aA7cuEjY8pgLzddK8r-mg7p_MnXj8'
+organization = 'thanawanphansa2020'
+database = 'msubotdb'
 
-# Heroku app URL
-heroku_app_url = 'https://msubotserver-8a9bb5aee729.herokuapp.com'
+# PlanetScale API base URL
+planet_scale_api_url = f'https://api.planetscale.com/v1/organizations/{organization}/databases/{database}'
+
+# Set the API key in the headers
+headers = {
+    'Authorization': f'Bearer {planet_scale_api_key}',
+    'Content-Type': 'application/json',
+}
 
 @app.route('/fetchData', methods=['GET'])
 def fetch_data():
     try:
-        # Use the PlanetScale connection to execute the query
-        query = text('SELECT * FROM serials')  # Replace 'your_table' with your actual table name
-        with engine.connect() as connection:
-            result = connection.execute(query)
+        # Example GET request to fetch data
+        response = requests.get(f'{planet_scale_api_url}/tables/serials', headers=headers)
 
-        # Return the result as JSON
-        return jsonify(result.fetchall())
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            return jsonify(response.json())
+        else:
+            return jsonify({'error': f'Failed to fetch data. Status Code: {response.status_code}'}), response.status_code
+
     except Exception as e:
         print(f"Error fetching data from PlanetScale: {str(e)}")
         return jsonify({'error': 'Internal Server Error'}), 500
 
-@app.route('/sendData', methods=['POST'])
-def send_data():
-    try:
-        # Assuming request data is JSON
-        data_to_send = request.get_json()
+# Add other routes and functions as needed
 
-        # Make a POST request to the Heroku app
-        heroku_response = requests.post(f"{heroku_app_url}/receiveData", json=data_to_send)
-
-        return jsonify({'message': 'Data sent to Heroku successfully', 'herokuResponse': heroku_response.json()})
-    except Exception as e:
-        print(f"Error sending data to Heroku: {str(e)}")
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-# Use the 'PORT' environment variable provided by Heroku for dynamic port binding
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use port 5000 if not provided
-    app.run(debug=False, port=port)
+    app.run(debug=False)
